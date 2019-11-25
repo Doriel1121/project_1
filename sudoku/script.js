9220401bf4aa1e241f7f73e73addaf9bc3771569
Doriel aboya <doriel494@gmail.com>
2019-11-25 18:42:38 +0200

    updated login page

--- sudoku/script.js
+++ sudoku/script.js
@@ -1,20 +1,16 @@
-function logInIdent(){
+function logInIdent(){ // function that check if the user name and the password are correct.
     let userName= document.getElementById("login_user").value;
     let password=document.getElementById("login_pass").value;
-    if(userName == 'abcd'){
-        return true;
+    
+    if(userName =='abcd' && password =='1234'){
+        location.replace("file:///C:/Users/Teacher/Desktop/Doriel/git/StudyCourse/sudoku/difficulty.html")
     }
-    else{
+    if(userName != 'abcd'){
         document.getElementById("error-name").innerHTML="wrong user name";
     }
-    if(password == '1234'){
-        return true;
+    if(password != '1234'){
+        document.getElementById("error-pass").innerHTML="wrong password";
+
     }
-        else{
-            document.getElementById("error-pass").innerHTML="wrong password";
-        
-    }
-    if(userName=='abcd' && password=='1234'){
-        
-    }
+    
 }
