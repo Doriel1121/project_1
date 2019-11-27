23093082c13c8b3166f0fddd3f232fea50fe4157
omer-mor <58226335+omer-mor@users.noreply.github.com>
2019-11-28 00:19:41 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,12 @@
+
+    function play(){
+        if(document.getElementById('push1').clicked == true){
+            location.replace("C:\Users\USER\Desktop\project3\game.html");
+        }
+        if(document.getElementById('push2').clicked == true){
+            location.replace("C:\Users\USER\Desktop\project3\game.html");
+        }
+        if(document.getElementById('push3').clicked == true){
+            location.replace("C:\Users\USER\Desktop\project3\game.html");
+        }
+    }
