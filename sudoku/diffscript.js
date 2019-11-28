92571bb08d65090efedc0c005d609f1a7b5572f3
Doriel aboya <doriel494@gmail.com>
2019-11-28 05:32:44 +0200

    matrix built

---
+++ sudoku/diffscript.js
@@ -0,0 +1,13 @@
+
+    function playEasy(){ // open the game at the easy level
+        
+            location.replace("file:///C:/Users/Teacher/Desktop/Doriel/git/project_1/sudoku/game.html");
+    }
+    function playMedium(){//open the game on the medium level
+        
+        location.replace("file:///C:/Users/Teacher/Desktop/Doriel/git/project_1/sudoku/game.html");
+}
+function playHard(){ //open the game on the hard level
+        
+    location.replace("file:///C:/Users/Teacher/Desktop/Doriel/git/project_1/sudoku/game.html");
+}
--- sudoku/script.js
+++ sudoku/script.js
@@ -2,13 +2,13 @@ function logInIdent(){ // function that check if the user name and the password 
     let userName= document.getElementById("login_user").value;
     let password=document.getElementById("login_pass").value;
     
-    if(userName =='abcd' && password =='1234'){
-        location.replace("file:///C:/Users/Teacher/Desktop/Doriel/git/StudyCourse/sudoku/difficulty.html")
+    if(userName =='abcd' && password =='1234'){ // 
+        location.replace("file:///C:/Users/Teacher/Desktop/Doriel/git/project_1/sudoku/difficulty.html")
     }
-    if(userName != 'abcd'){
+    if(userName != 'abcd'){// condition for the username
         document.getElementById("error-name").innerHTML="wrong user name";
     }
-    if(password != '1234'){
+    if(password != '1234'){ // condition fot the password
         document.getElementById("error-pass").innerHTML="wrong password";
 
     }
